80ae85f8f890a13ec4941f66b0d1eaa5e5b929e5
Karnveer Ubhi <k.ubhi@outlook.com>
2022-03-12 21:06:54 -0500

    Initial commit

---
+++ src/GameSettings.java
@@ -0,0 +1,42 @@
+public class GameSettings {
+    private int range;
+    private int tries;
+    private int answer;
+    private int guess;
+
+    GameSettings(int range, int tries) {
+        this.range = range;
+        this.tries = tries;
+    }
+
+    public int getGuess() {
+        return guess;
+    }
+
+    public void setGuess(int guess) {
+        this.guess = guess;
+    }
+
+    public int getRange() {
+        return range;
+    }
+
+    public void setRange(int range) {
+        this.range = range;
+    }
+
+    public int getTries() {
+        return tries;
+    }
+
+    public void setTries(int tries) {
+        this.tries = tries;
+    }
+    public int getAnswer() {
+        return answer;
+    }
+
+    public void setAnswer(int answer) {
+        this.answer = answer;
+    }
+}
