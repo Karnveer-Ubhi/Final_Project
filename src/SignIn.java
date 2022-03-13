579a8ddc18d8a686f052c6bf17cbe8014f60d6bd
Karnveer Ubhi <k.ubhi@outlook.com>
2022-03-12 21:44:19 -0500

    Changes by porkchop

--- src/SignIn.java
+++ src/SignIn.java
@@ -1,3 +1,36 @@
+import java.io.File;  // Import the File class
+import java.io.IOException;  // Import the IOException class to handle errors
+import java.io.FileWriter;   // Import the FileWriter class;
 public class SignIn {
-    
+    public static void main(String[] args) {
+        try {
+            File myObj = new File("SignIn.txt");
+            if (myObj.createNewFile()) {
+                System.out.println("File created: " + myObj.getName());
+            } else {
+                System.out.println("File already exists.");
+            }
+        } catch (IOException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+
+        try {
+            FileWriter myWriter = new FileWriter("SignIn.txt");
+            myWriter.write("We will change this to read the textbox in the andriod studio not able to get input from user here");
+            myWriter.close();
+            System.out.println("Successfully wrote to the file.");
+        } catch (IOException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+    }
 }
+
+
+
+
+
+
+
+
